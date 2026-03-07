45b40970153f1b25a90ecb6ae101b35e81c76ac3
hxrmn45 <harmanpassi45@gmail.com>
2026-03-07 21:47:29 +0530

    Update import paths to use relative paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "sonner";
 
-import { PageLayout } from "@/components/layout/PageLayout";
-import { ProtectedRoute } from "@/components/common/ProtectedRoute";
-import { AuthProvider } from "@/context/AuthContext";
-import AuthPage from "@/pages/AuthPage";
-import CommunityPage from "@/pages/CommunityPage";
-import EventsPage from "@/pages/EventsPage";
-import GamesPage from "@/pages/GamesPage";
-import HomePage from "@/pages/HomePage";
+import { PageLayout } from "./components/layout/PageLayout";
+import { ProtectedRoute } from "./components/common/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext";
+import AuthPage from "./pages/AuthPage";
+import CommunityPage from "./pages/CommunityPage";
+import EventsPage from "./pages/EventsPage";
+import GamesPage from "./pages/GamesPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -19,12 +19,34 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/auth" element={<AuthPage />} />
-            <Route path="/events" element={<ProtectedRoute><EventsPage /></ProtectedRoute>} />
-            <Route path="/games" element={<ProtectedRoute><GamesPage /></ProtectedRoute>} />
-            <Route path="/community" element={<ProtectedRoute><CommunityPage /></ProtectedRoute>} />
+            <Route
+              path="/events"
+              element={
+                <ProtectedRoute>
+                  <EventsPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/games"
+              element={
+                <ProtectedRoute>
+                  <GamesPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/community"
+              element={
+                <ProtectedRoute>
+                  <CommunityPage />
+                </ProtectedRoute>
+              }
+            />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </PageLayout>
+
         <Toaster richColors position="top-right" />
       </AuthProvider>
     </BrowserRouter>
